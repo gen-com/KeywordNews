511db239901b6f6a7b7cfa09b75d0d0142ba84ff
Koo <31850425+gen-com@users.noreply.github.com>
2024-05-02 22:14:39 +0900

    Update persistence logic
    modified the persistence protocol to provide functionality rather than
    simply providing storage access.

--- KeywordNews/Persist/Abstractions/Persistable.swift
+++ KeywordNews/Persist/Abstractions/Persistable.swift
@@ -7,8 +7,31 @@
 
 import CoreData
 
-/// 디스크에 저장가능한 유형을 정의합니다.
 protocol Persistable {
-    /// 저장소.
-    var container: NSPersistentContainer { get }
+    /// 데이터를 추가합니다.
+    /// - Parameters:
+    ///   - entityName: 데이터 타입 이름.
+    ///   - managedObjectHandler: 저장할 객체를 처리하는 함수. 저장할 객체를 모두 설정했다면 true를 반환해야 합니다.
+    func add(
+        _ entityName: String,
+        amount: Int,
+        with managedObjectHandler: @escaping (NSManagedObject) -> Bool
+    ) async throws
+    /// 데이터를 찾아서 불러옵니다.
+    /// - Parameter request: 찾기 위한 요청.
+    /// - Returns: 찾은 데이터.
+    func fetch<T: NSManagedObject>(with request: NSFetchRequest<T>) async throws -> [T]
+    /// 데이터를 최신화힙니다.
+    /// - Parameters:
+    ///   - entity: 데이터 타입.
+    ///   - predicate: 최신화할 데이터 찾는 조건.
+    ///   - propertiesToUpdate: 최신화할 속성 딕셔너리.
+    func update(
+        _ entityName: String,
+        forMatching predicate: NSPredicate?,
+        with propertiesToUpdate: [AnyHashable: Any]
+    ) async throws
+    /// 데이터를 삭제합니다.
+    /// - Parameter request: 찾기 위한 요청.
+    func remove<T: NSManagedObject>(with request: NSFetchRequest<T>) async throws
 }
--- KeywordNews/Persist/PersistenceController.swift
+++ KeywordNews/Persist/PersistenceController.swift
@@ -7,29 +7,138 @@
 
 import CoreData
 
-/// 디스크 저장을 관리하는 구현체입니다.
-final class PersistenceController: Persistable {
-    let container: NSPersistentContainer
-    
-    // MARK: - Type Property
-    
-    /// 저장소 이름.
-    private static let containerName = "KeywordNews"
+final class PersistenceController {
+    /// 저장 관리 컨테이너.
+    private let container: NSPersistentContainer
+    /// 저장 관리 문맥.
+    private let context: NSManagedObjectContext
     
     // MARK: - Initializer
     
     /// 디스크 저장을 관리자를 생성합니다.
     /// - Parameter inMemory: 메모리에 저장할 지 여부를 결정합니다.
     init(inMemory: Bool = false) {
-        container = NSPersistentContainer(name: PersistenceController.containerName)
+        container = NSPersistentContainer(name: Constant.containerName)
         if inMemory, let persistentStoreDescription = container.persistentStoreDescriptions.first {
-            persistentStoreDescription.url = URL(fileURLWithPath: "/dev/null")
+            persistentStoreDescription.url = URL(fileURLWithPath: Constant.inMemoryPath)
         }
-        container.loadPersistentStores { (storeDescription, error) in
+        container.loadPersistentStores { (_, error) in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         }
         container.viewContext.automaticallyMergesChangesFromParent = true
+        context = container.newBackgroundContext()
+    }
+}
+
+// MARK: - Persistable conformance
+
+extension PersistenceController: Persistable {
+    func add(
+        _ entityName: String,
+        amount: Int,
+        with managedObjectHandler: @escaping (NSManagedObject) -> Bool
+    ) async throws {
+        try await context.perform { [weak self] in
+            guard let weakSelf = self else { throw CommonError.valueNotFound }
+            let batchInsertRequest = NSBatchInsertRequest(
+                entityName: entityName,
+                managedObjectHandler: managedObjectHandler
+            )
+            batchInsertRequest.resultType = .count
+            guard let insertResult = try weakSelf.context.execute(batchInsertRequest) as? NSBatchInsertResult,
+                  let insertedCount = insertResult.result as? NSInteger
+            else { throw Error.failedToAdd(entityName: entityName, failedAmount: amount) }
+            guard amount == insertedCount
+            else { throw Error.failedToAdd(entityName: entityName, failedAmount: amount - insertedCount) }
+        }
+    }
+    
+    func fetch<T>(with request: NSFetchRequest<T>) async throws -> [T] {
+        try await context.perform { [weak self] in
+            guard let weakSelf = self else { throw CommonError.valueNotFound }
+            do { return try weakSelf.context.fetch(request) }
+            catch { throw Error.failedToFetch }
+        }
+    }
+    
+    func update(
+        _ entityName: String,
+        forMatching predicate: NSPredicate?, 
+        with propertiesToUpdate: [AnyHashable : Any]
+    ) async throws {
+        try await context.perform { [weak self] in
+            guard let weakSelf = self else { throw CommonError.valueNotFound }
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: entityName)
+            fetchRequest.predicate = predicate
+            guard let amount = try? weakSelf.context.count(for: fetchRequest) else { throw Error.failedToFetch }
+            let batchUpdateRequest = NSBatchUpdateRequest(entityName: entityName)
+            batchUpdateRequest.predicate = predicate
+            batchUpdateRequest.propertiesToUpdate = propertiesToUpdate
+            batchUpdateRequest.resultType = .updatedObjectsCountResultType
+            guard let updatedResult = try weakSelf.context.execute(batchUpdateRequest) as? NSBatchUpdateResult,
+                  let updatedCount = updatedResult.result as? Int
+            else { throw Error.failedToUpdate(entityName: entityName, failedAmount: amount) }
+            guard amount == updatedCount
+            else { throw Error.failedToUpdate(entityName: entityName, failedAmount: amount - updatedCount) }
+        }
+    }
+    
+    func remove<T>(with request: NSFetchRequest<T>) async throws {
+        try await context.perform { [weak self] in
+            guard let weakSelf = self else { throw CommonError.valueNotFound }
+            guard let fetchRequest = request as? NSFetchRequest<NSFetchRequestResult>
+            else { throw Error.failedToRemove }
+            fetchRequest.fetchBatchSize = Constant.batchSize
+            let removeRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+            removeRequest.resultType = .resultTypeStatusOnly
+            guard let result = try? weakSelf.context.execute(removeRequest) as? NSBatchDeleteResult,
+                  let success = result.result as? Bool, success
+            else { throw Error.failedToRemove }
+        }
+    }
+}
+
+// MARK: - Error
+
+extension PersistenceController {
+    enum Error: PresentableError {
+        /// 데이터를 추가하지 못한 경우.
+        case failedToAdd(entityName: String, failedAmount: Int)
+        /// 데이터를 가져오지 못한 경우.
+        case failedToFetch
+        /// 데이터를 수정하지 못한 경우.
+        case failedToUpdate(entityName: String, failedAmount: Int)
+        /// 데이터를 삭제하지 못한 경우.
+        case failedToRemove
+        
+        var title: String {
+            "저장소 오류"
+        }
+        
+        var description: String {
+            switch self {
+            case let .failedToAdd(entityName, amount):
+                return "중복 혹은 다른 이유로 \(amount)개의 \(entityName.lowercased()) 데이터 추가에 실패했습니다."
+            case .failedToFetch:
+                return "데이터를 가져오는데 실패했습니다."
+            case let .failedToUpdate(entityName, amount):
+                return "\(amount)개의 \(entityName.lowercased()) 데이터 변경에 실패했습니다."
+            case .failedToRemove:
+                return "데이터 삭제에 실패했습니다."
+            }
+        }
+    }
+}
+
+// MARK: - Contants
+
+extension PersistenceController {
+    private enum Constant {
+        static let containerName = "KeywordNews"
+        static let inMemoryPath = "/dev/null"
+        
+        static let batchSize = 100
     }
 }
